1785c501a6a3c5f1a66262a94c17c9e103d2ebdd
CODE-CRUSHERS <codcrushers0@gmail.com>
2024-12-03 19:13:07 +0530

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,17 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDLrFF01EHt_KgnUASN7kGqR8RDWLY2W-s",
+    authDomain: "gobarshakti-sih.firebaseapp.com",
+    projectId: "gobarshakti-sih",
+    storageBucket: "gobarshakti-sih.firebasestorage.app",
+    messagingSenderId: "109279329962",
+    appId: "1:109279329962:web:e4c7183cfacf65101cdad3",
+  };
+  
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Initialize Firestore
+  const db = firebase.firestore();
+  
